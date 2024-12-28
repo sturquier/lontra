6a48f87b5f5742f9e5d33fd1c42f16681337d3e2
sturquier <turquiersimon@hotmail.fr>
2024-12-28 18:14:49 +0100

    Improve tags rendering

--- lib/components/tabs/profile/tags/tags.tsx
+++ lib/components/tabs/profile/tags/tags.tsx
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+import Image from 'next/image';
 import { useForm, SubmitHandler } from 'react-hook-form';
 import { zodResolver } from "@hookform/resolvers/zod";
 import { z, ZodType } from "zod";
@@ -7,6 +9,11 @@ import { CreateTagPayload } from '@models/tag';
 import { useFetchTagsQuery } from '@store/features/tags/tags.query';
 import { Button, FormInput, Loader, Tag } from '@components/index';
 import styles from './tags.module.scss';
+
+enum SECTION_KEY {
+  LIST = 'list',
+  FORM = 'form'
+}
 
 const CreateTagSchema: ZodType<CreateTagPayload> = z
   .object({
@@ -18,11 +25,22 @@ const CreateTagSchema: ZodType<CreateTagPayload> = z
   })
 
 export default function ProfileTagsTab () {
+  const [sections, setSections] = useState<{ key: SECTION_KEY, isOpened: boolean }[]>([
+    { key: SECTION_KEY.LIST, isOpened: true },
+    { key: SECTION_KEY.FORM, isOpened: false }
+  ])
+
   const { data: tags, isFetching, refetch } = useFetchTagsQuery();
 
   const { register, reset, handleSubmit, formState: { isValid, errors } } = useForm<CreateTagPayload>({
     resolver: zodResolver(CreateTagSchema)
   })
+
+  const toggleSection = (key: SECTION_KEY): void => {
+    setSections(sections.map(section => section.key === key ? { ...section, isOpened: !section.isOpened } : { ...section, isOpened: false }))
+  }
+
+  const isOpenedSection = (key: SECTION_KEY): boolean => sections.find(section => section.key === key)?.isOpened ?? false;
 
   const createTag: SubmitHandler<CreateTagPayload> = async (payload) => {
     const { label } = payload;
@@ -53,36 +71,72 @@ export default function ProfileTagsTab () {
     <div className={styles['profile-tags']}>
       <h2 className={styles['profile-tags-title']}>Tags</h2>
       <div className={styles['profile-tags-content']}>
-        <div className={styles['profile-tags-content-creation']}>
-          <div className={styles['profile-tags-content-creation-subtitle']}>Create a tag</div>
-          <form className={styles['profile-tags-content-creation-form']} onSubmit={handleSubmit(createTag)}>
-            <div className={styles['profile-tags-content-creation-form-row']}>
-              <FormInput
-                placeholder='Label'
-                icon={{
-                  src: '/icons/tag.svg',
-                  alt: 'Tag icon'
-                }}
-                register={register('label')}
+        <div className={styles['profile-tags-content-toggles']}>
+          <div 
+            className={styles['profile-tags-content-toggles-toggle']}
+            onClick={(): void => toggleSection(SECTION_KEY.LIST)}
+          >
+            <div className={styles['profile-tags-content-toggles-toggle-icon']}>
+              <Image
+                src={'/icons/caret-right.svg'}
+                alt={'Caret right icon'}
+                width={24}
+                height={24}
               />
-              <div className={styles['profile-tags-content-creation-form-row-error']}>
-                {errors.label && <span className={styles['profile-tags-content-creation-form-row-error-text']}>{errors.label.message}</span>}
-              </div>
             </div>
-            <Button type='submit' disabled={!isValid} className='submit'>CREATE</Button>
-          </form>
+            <h4 className={`${styles['profile-tags-content-toggles-toggle-title']} ${isOpenedSection(SECTION_KEY.LIST) ? styles['profile-tags-content-toggles-toggle-title-active'] : ''}`}>List existing tags</h4>
+          </div>
+          <div 
+            className={styles['profile-tags-content-toggles-toggle']} 
+            onClick={(): void => toggleSection(SECTION_KEY.FORM)}
+          >
+            <div className={styles['profile-tags-content-toggles-toggle-icon']}>
+              <Image
+                src={'/icons/caret-right.svg'}
+                alt={'Caret right icon'}
+                width={24}
+                height={24}
+              />
+            </div>
+            <h4 className={`${styles['profile-tags-content-toggles-toggle-title']} ${isOpenedSection(SECTION_KEY.FORM) ? styles['profile-tags-content-toggles-toggle-title-active'] : ''}`}>Create a new tag</h4>
+          </div>
         </div>
-        <div className={styles['profile-tags-content-list']}>
-          <div className={styles['profile-tags-content-list-subtitle']}>Tags list</div>
-          {isFetching ? (
-            <div className={styles['profile-tags-content-list-loader']}>
-              <Loader />
+        <div className={styles['profile-tags-content-section']}>
+          {isOpenedSection(SECTION_KEY.LIST) && (
+            <div className={styles['profile-tags-content-section-list']}>
+              <h3 className={styles['profile-tags-content-section-list-subtitle']}>List existing tags</h3>
+              {isFetching ? (
+                <div className={styles['profile-tags-content-section-list-loader']}>
+                  <Loader />
+                </div>
+              ) : (
+                <div className={styles['profile-tags-content-section-list-rows']}>
+                  {tags?.map((tag) => (
+                    <Tag key={tag.id} onDeleteCallback={(): Promise<void> => deleteTag(tag.id)}>{tag.label}</Tag>
+                  ))}
+                </div>
+              )}
             </div>
-          ) : (
-            <div className={styles['profile-tags-content-list-rows']}>
-              {tags?.map((tag) => (
-                <Tag key={tag.id} onDeleteCallback={(): Promise<void> => deleteTag(tag.id)}>{tag.label}</Tag>
-              ))}
+          )}
+          {isOpenedSection(SECTION_KEY.FORM) && (
+            <div className={styles['profile-tags-content-section-create']}>
+              <h3 className={styles['profile-tags-content-section-create-subtitle']}>Create a new tag</h3>
+              <form className={styles['profile-tags-content-section-create-form']} onSubmit={handleSubmit(createTag)}>
+                <div className={styles['profile-tags-content-section-create-form-row']}>
+                  <FormInput
+                    placeholder='Label'
+                    icon={{
+                      src: '/icons/tag.svg',
+                      alt: 'Tag icon'
+                    }}
+                    register={register('label')}
+                  />
+                  <div className={styles['profile-tags-content-section-create-form-row-error']}>
+                    {errors.label && <span className={styles['profile-tags-content-section-create-form-row-error-text']}>{errors.label.message}</span>}
+                  </div>
+                </div>
+                <Button type='submit' disabled={!isValid} className='submit'>CREATE</Button>
+              </form>
             </div>
           )}
         </div>
